e1e7447aca59bf002414bcfd9b84cdaae8c458a0
ERICK MAURICIO TIZNADO RODRIGUEZ <USIS038521@ugb.edu.sv>
2021-09-19 17:20:07 -0600

    Agregar archivos de proyecto.

---
+++ Loginn/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace Loginn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+          
+
+
+            InitializeComponent();
+        }
+
+        SqlConnection connection = new SqlConnection("Data Source=DESKTOP-NV76SQV;Initial Catalog=Loginn;Integrated Security=True");
+        public void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Inicio ini = new Inicio();
+            OleDbConnection conexion = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\DELL\Documents\Usuario.accdb ");
+            OleDbCommand com = new OleDbCommand();
+            OleDbDataReader dtr;
+            conexion.Open();
+            com.Connection = conexion;
+            com.CommandType = CommandType.Text;
+            com.CommandText = "SELECT Contraseña FROM Usuarios WHERE nUsuario='" + txtUsuario.Text +"'";
+            try
+            {
+                dtr = com.ExecuteReader();
+                if (dtr.HasRows)
+                {
+                    while(dtr.Read())
+                    {
+                        if (dtr.GetValue(0).ToString() == txtContraseña.Text)
+                        {
+                            ini.Show();
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("A ocurrido un error");
+                        }
+                    }
+                }
+            }
+            catch(Exception k)
+            {
+                MessageBox.Show(k.ToString());
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-NV76SQV;Initial Catalog=Loginn;Integrated Security=True");
+            connection.Open();
+            SqlCommand comd = new SqlCommand("SELECT Contraseña FROM Users WHERE USuario= @vusuario AND Contraseña= @vContrasena",connection);
+            comd.Parameters.AddWithValue("@vusuario",txtUsuario.Text);
+            comd.Parameters.AddWithValue("@vContrasena", txtContraseña.Text);
+            SqlDataReader lector = comd.ExecuteReader();
+            if (lector.Read());
+            {
+                MessageBox.Show("Contraseña correcta");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 res = new Form2();
+            res.Show();
+        }
+    }
+}
---
+++ Loginn/Form2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Loginn
+{
+    public partial class Form2 : Form
+    {
+		Contad obj = new Contad();
+		public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			try 
+			{
+				OleDbConnection conexion = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\DELL\Documents\Usuario.accdb ");
+				conexion.Open();
+				int Id = obj.cont() + 1;
+				string insertar = "INSERT INTO Usuarios  VALUES ( @Id,@Usuario, @Cont)";
+				OleDbCommand cmd = new OleDbCommand(insertar, conexion);
+				cmd.Parameters.AddWithValue("@Id", Convert.ToInt32(txtId.Text));
+				cmd.Parameters.AddWithValue("@Usuario", txtUsuario.Text);
+				cmd.Parameters.AddWithValue("@Cont", txtContraseña.Text);
+				cmd.ExecuteNonQuery();
+				
+			}
+
+			catch (DBConcurrencyException ex)
+			{
+				MessageBox.Show("Error de concurrencia:\n" + ex.Message);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+	}
+}
